b71886321a681f8a1d980af773911edaef85ff1c
LeonardoMendes89 <leoprogressivo@hotmail.com>
2022-02-20 11:40:29 -0300

    master

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const log     = require('morgan')
 const app     = express()
 
 app.use(express.urlencoded({extended:true}))
-app.use(cors({origin:'*', credentials: true }))
+app.use(cors({origin:'https://cloudweek.netlify.app/', credentials: true }))
 app.use(express.json())
 app.use(log('dev'))
 
