f1a264f40fe0f37474d1ef810791ac67061e49ab
LeonardoMendes89 <leoprogressivo@hotmail.com>
2022-02-20 12:56:08 -0300

    master

--- index.js
+++ index.js
@@ -9,32 +9,13 @@ const log     = require('morgan')
 const app     = express()
 
 app.use(express.urlencoded({extended:true}))
+app.use(cors())
 app.use(express.json())
 app.use(log('dev'))
 
 app.use('/', server)
 
-// app.use(cors({origin:'https://cloudweek.netlify.app/', credentials: true }))
 
-// app.use(cors('*'))
-
-// app.use(cors())
-
-// app.use((req, res, next) => {
-//     res.header("Access-Control-Allow-Origin", "*")
-//     res.header(
-//       "Access-Control-Allow-Headers",
-//       "Origin, X-Requested, Content-Type, Accept Authorization"
-//     )
-//     if (req.method === "OPTIONS") {
-//       res.header(
-//         "Access-Control-Allow-Methods",
-//         "POST, PUT, PATCH, GET, DELETE"
-//       )
-//       return res.status(200).json({})
-//     }
-//     next()
-// })
 
 app.listen(process.env.PORT || _PORT,()=>{
     console.log(`Online into Port: ${_PORT}`)
