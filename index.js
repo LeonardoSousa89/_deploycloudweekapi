1bb8a3f38b43a04c0b8820edd4f4feacd85bf900
LeonardoMendes89 <leoprogressivo@hotmail.com>
2022-02-20 12:05:44 -0300

    master

--- index.js
+++ index.js
@@ -15,22 +15,22 @@ app.use(log('dev'))
 app.use('/', server)
 
 // app.use(cors({origin:'https://cloudweek.netlify.app/', credentials: true }))
-// app.use(cors('*'))
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*")
-    res.header(
-      "Access-Control-Allow-Headers",
-      "Origin, X-Requested, Content-Type, Accept Authorization"
-    )
-    if (req.method === "OPTIONS") {
-      res.header(
-        "Access-Control-Allow-Methods",
-        "POST, PUT, PATCH, GET, DELETE"
-      )
-      return res.status(200).json({})
-    }
-    next()
-})
+app.use(cors('*'))
+// app.use((req, res, next) => {
+//     res.header("Access-Control-Allow-Origin", "*")
+//     res.header(
+//       "Access-Control-Allow-Headers",
+//       "Origin, X-Requested, Content-Type, Accept Authorization"
+//     )
+//     if (req.method === "OPTIONS") {
+//       res.header(
+//         "Access-Control-Allow-Methods",
+//         "POST, PUT, PATCH, GET, DELETE"
+//       )
+//       return res.status(200).json({})
+//     }
+//     next()
+// })
 
 app.listen(process.env.PORT || _PORT,()=>{
     console.log(`Online into Port: ${_PORT}`)
